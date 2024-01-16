b7b24626b129ca3f740e2835ef181f3220118b2a
Owen Lucier <owenlucier@gmail.com>
2024-01-15 22:44:43 -0500

    Added responsive elements to about section

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -7,22 +7,27 @@ const AboutSection = () => {
     return (
         <section id="About" className="pb-20">
             <Container>
+            
+            <h2 className="font-extrabold justify-center sm:justify-left text-4xl py-20 my-8 flex md:py-8">About Me</h2>
+
             <div className="grid grid-cols-1 md:grid-cols-12 pb-8 mb-8">
-                <div className="col-span-12 place-self-center text-center md:text-left ">
-                    <h2 className="font-extrabold text-4xl py-20 my-8 flex md:py-8">About Me</h2>
-                </ div>
-                <div className="col-span-6">
-                    <p className="text-gray-500 text-base text-center md:text-left sm:text-lg lg:text-xl mb-6 sm:mr-8">
+
+                <div className="col-span-12 md:col-span-8">
+                    <p className="text-gray-500 text-base text-center md:text-left sm:text-lg lg:text-xl md:pr-6 ">
                         I am a 4th year User Experience Design student at Wifrid Laurier University currently working as a UI Designer in a co-op role. I am passionate about creating beautiful and functional designs with a focus on the Human Centered Design Approach. I look to expand and improve my skills in the field of UX Design and am always seeking new opportunities to learn and grow.
                     </p>
                 </div>
-                <div className="col-span-6">
+
+                <div className="col-span-12 md:col-span-4">
+                    <div className="mt-10 md:mt-2">   
                     <ProgressBar title="Figma" href="/figma.png" progress={90}  />
                     <ProgressBar title="Git" href="/git.png" progress={40}  />
                     <ProgressBar title="TypeScript" href="/typescript.png" progress={40}  />
                     <ProgressBar title="React" href="/react.png" progress={60}  />
                     <ProgressBar title="Javascript" href="/javascript.png" progress={70}  />
+                    </ div>
                 </div>
+                
             </div>
             </Container>
         </section>
--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,11 +1,9 @@
 import { PropsWithChildren } from "react"
 
-const myClasses =  "whatever bg-red-500"
-
 type ContainerProps = PropsWithChildren & {
   classes?: string
 }
 
 export const Container: React.FC<ContainerProps> = ({ children, classes }) => {
-  return <div className={`container mx-auto px-4 ${classes}`}>{children}</div>
+  return <div className={`container mx-auto px-8 ${classes}`}>{children}</div>
 }
