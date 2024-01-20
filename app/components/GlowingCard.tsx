de5c79f103784231bee46906de35a73dbfe568cf
Owen Lucier <owenlucier@gmail.com>
2024-01-20 18:37:10 -0500

    Added responsiveness

--- app/components/GlowingCard.tsx
+++ app/components/GlowingCard.tsx
@@ -10,10 +10,10 @@ type ContainerProps = PropsWithChildren & {
 
   const GlowingCard: React.FC<ContainerProps> = ({ children, classes, style }) => {
     return (
-        <div className="w-[660px] h-[230px] bg-transparent relative">
-            <div className="absolute inset-0 flex bg-gradient-to-tr from-transparent via-white to-transparent rounded-2xl" 
+        <div className="w-full bg-transparent relative" style={{ paddingBottom: '40%' }}>
+            <div className="absolute inset-0 flex bg-[radial-gradient(at_center_top,_#FFFFFF,_#565656,_transparent)] rounded-lg sm:rounded-2xl" 
                  style={{ boxShadow: '-13px 39px 250px -39px rgba(255, 255, 255, 0.17)' }}>
-                <div className={`flex-grow m-0.5 bg-[#181818] rounded-2xl ${classes}`}>
+                <div className={`flex-grow m-0.5 bg-[#181818] rounded-lg sm:rounded-2xl ${classes}`}>
                     {style}{children}
                 </div>
             </div>
@@ -22,4 +22,7 @@ type ContainerProps = PropsWithChildren & {
 };
 
 export default GlowingCard;
+
+
+
 
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { FaEye, FaCode } from "react-icons/fa6";
 import GlowingCard from './GlowingCard';
 import Image from 'next/image'
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 
 type ProjectCardProps = {
@@ -17,28 +19,33 @@ type ProjectCardProps = {
 const ProjectCard = ({ id, imgUrl, title, description, tag }: ProjectCardProps) => {
     return (
         <GlowingCard>
-            <div className="grid h-full grid-cols-2 gap-10 p-3">
+            <div className="flex h-full p-3 md:p-6 ">
 
-                {/*Image */}
-                <div className="flex col-span-1 h-flex bg-transparent relative">
-                    <div className="absolute inset-0 flex bg-gradient-to-tr from-transparent via-white to-transparent rounded-2xl"
+                {/* Image */}
+                <div className="flex-1 bg-transparent relative">
+                    <div className="absolute inset-0 flex bg-[radial-gradient(at_top_right,_#FFFFFF,_#565656,_transparent)] rounded-lg sm:rounded-2xl"
                         style={{ boxShadow: '-13px 39px 250px -39px rgba(255, 255, 255, 0.17)' }}>
-                        <div className={`flex-grow m-0.5 bg-[#181818] rounded-2xl`}>
+                        <div className="flex-grow m-0.5 bg-[#181818] rounded-lg sm:rounded-2xl">
                             <div
-                                className="col-span-1 h-full rounded-t-xl relative  group rounded-b-2xl"
+                                className="h-full rounded-t-xl relative group rounded-b-2xl"
                                 style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
                             />
                         </div>
                     </div>
                 </div>
-                
 
-                <div className="col-span-1">
-                    <h5 className="font-xl font-semibold mb-2">{title}</h5>
-                    <p className="text-gray-200">{description}</p>
+                <div className="flex justify-top flex-col flex-1 ml-5">
+                    {/* Align the arrow to the end */}
+                    <div className="flex items-center justify-between mb-4">
+                        <h5 className="text-lg sm:text-xl font-semibold">{title}</h5>
+                        <FaArrowRightLong className="hidden sm:flex items-center text-white text-2xl" />
+                    </div>
+
+                    <p className="hidden sm:flex xs:text-xs md:text-lg text-gray-200">{description}</p>
+
                     <div className="flex flex-wrap mt-4">
                         {/* Make an individual pill component */}
-                        {/*<div className="bg-transparent border-2 border-emerald-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">{tag}</div>/ */}
+                        {/* <div className="bg-transparent border-2 border-emerald-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">{tag}</div> */}
                     </div>
                 </div>
             </div>
