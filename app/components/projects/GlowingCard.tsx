281ccc0c44600056b433d41ec7ad4a672ec3a1c9
Owen Lucier <owenlucier@gmail.com>
2024-01-29 02:13:38 -0500

    Refactor GlowingCard component styles

--- app/components/projects/GlowingCard.tsx
+++ app/components/projects/GlowingCard.tsx
@@ -18,7 +18,8 @@ const GlowingCard: React.FC<ContainerProps> = ({ children, classes, style, link}
             >
             
             <div className="absolute inset-0 flex bg-[radial-gradient(at_center_top,_#FFFFFF,_#565656,_transparent)] rounded-lg sm:rounded-2xl"
-                style={{ boxShadow: '-13px 3px 250px -39px rgba(255, 255, 255, 0.17)' }}>
+                // style={{ boxShadow: '-13px 3px 250px -39px rgba(255, 255, 255, 0.17)' }}
+                >
                 <div className={`flex-grow m-0.5 bg-[#181818] rounded-lg sm:rounded-2xl ${classes}`}>
                     {style}{children}
                 </div>
