575436e3b80d20b21cabe2ffe9d190dbabcca352
Owen Lucier <owenlucier@gmail.com>
2024-01-29 01:45:47 -0500

    Remove vertical progress bar from ProjectsSection

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -48,14 +48,6 @@ const ProjectsSection = () => {
 
                 <div className="grid grid-cols-3">
 
-                    <div className="flex col-span-1 justify-end pt-[100px] pr-20">
-                        {/*A component that takes project props and creates a verticle progress bar with points at each project card and title/date of project*/}
-                        <div className="w-[2px] h-full bg-emerald-500 rounded-full">
-                            <div className="h-4 w-4 bg-emerald-500 rounded-full -translate-x-[7px]"/>
-                        </div>
-
-                    </div>
-
                     <div className="col-span-2">
                         {projectData.map((project) => (
                             <ProjectCard
