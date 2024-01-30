cdd1f0b7b60ecc360e18f9fa02141d6c8cc6ace6
Owen Lucier <owenlucier@gmail.com>
2024-01-30 16:53:51 -0500

    Updated Next.js and created new project components

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React from 'react'
 import ProjectCard from './projects/ProjectCard';
-import {motion, useScroll } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 
-// Rest of the code...
 import { Container } from './Container';
+
 
 const projectData = [
     {
@@ -13,7 +13,9 @@ const projectData = [
         description: `A personal project aimed at learning and expanding my skills with Typescript, React, NextJs, Tailwind and Framer Motion; while also showcasing my work and skills! This project is ongoing and regular updates are made. \n `,
         imgUrl: "/portfolio.png",
         tags: ["UX Design", "UI Design", "NextJs", "React", "Typescript", "Tailwind", "Framer Motion"],
-        link: "/project-1"
+        link: "/project-1",
+        subtitle: "Personal Project",
+        date: "January - 2024"
     },
     {
         id: 2,
@@ -21,7 +23,8 @@ const projectData = [
         description: `A joint-case study with Unless Design Partners, a UX Design agency based in Kitchener, Ontario. My team and I conducted a UX Research study on the Wilfrid Laurier University wellness services and proposed a new design solution. Our contributions awarded us 1st place against 5 other teams! `,
         imgUrl: "/grd-cover.png",
         tags: ["UX Design", "UI Design", "Figma", "UX Research", "UX Strategy"],
-        link: "/project-2"
+        link: "/project-2",
+        date: "April - 2022"
     },
     {
         id: 3,
@@ -43,14 +46,12 @@ const ProjectsSection = () => {
         <section id="Projects" className="flex min-h-screen">
             <Container>
 
-            <h2 className="font-extrabold text-4xl py-2 mt-20 flex justify-left text-white justify-center">My Projects</h2>
-            <div className="md:hidden">
-                <h1 className="text-white text-4xl text-center pt-40">Mobile project view under construction</h1>
-            </div>
+                <div className="flex flex-wrap">
+                    <h2 className="font-extrabold text-xl mx-8 py-2 mt-20 text-white justify-start">My Projects</h2>
+                </div>
+                <div className=" md:flex flex-col">
 
-                <div className="hidden md:grid grid-cols-2">
-
-                    <div className="col-span-2 px-32">
+                    <div>
                         {projectData.map((project) => (
                             <ProjectCard
                                 key={project.id}
@@ -60,12 +61,12 @@ const ProjectsSection = () => {
                                 imgUrl={project.imgUrl}
                                 tag={project.tags}
                                 link={project.link}
-                                />
+                                subtitle={project.subtitle}
+                                date={project.date}
+                            />
                         ))}
                     </div>
                 </div>
-
-
 
             </Container>
         </ section >
--- app/components/projects/ProjectCard.tsx
+++ app/components/projects/ProjectCard.tsx
@@ -14,20 +14,21 @@ type ProjectCardProps = {
     description: string;
     tag: string[];
     link: string;
+    subtitle?: string;
+    date?: string;
 };
 
 // ProjectCard is a React functional component that accepts an imgUrl, title, description, and tag prop from the ProjectsSection 
-const ProjectCard = ({ id, imgUrl, title, description, tag, link }: ProjectCardProps) => {
+const ProjectCard = ({ id, imgUrl, title, subtitle, description, tag, link, date }: ProjectCardProps) => {
     return (
 
-        <div className="hidden md:block">
-            <GlowingCard link={link} >
+        <div className="">
+            {/* <GlowingCard link={link} >
                 <div className="flex h-full p-3 md:p-6 ">
-                    {/* Image */}
                     <div className="flex-1 bg-transparent relative">
                         <div className="absolute inset-0 flex rounded-lg sm:rounded-2xl"
-                           // style={{ boxShadow: '-13px 39px 250px -39px rgba(255, 255, 255, 0.17)' }}
-                           >
+                        // style={{ boxShadow: '-13px 39px 250px -39px rgba(255, 255, 255, 0.17)' }}
+                        >
                             <div className="flex-grow m-0.5 bg-[#181818] rounded-lg sm:rounded-2xl">
                                 <div
                                     className="h-full rounded-t-xl relative group rounded-b-2xl"
@@ -37,10 +38,8 @@ const ProjectCard = ({ id, imgUrl, title, description, tag, link }: ProjectCardP
                         </div>
                     </div>
                     <div className="flex flex-col flex-1 ml-5 md:pt-2 ">
-                        {/* Align the arrow to the end */}
                         <div className="flex items-center justify-between mb-1 lg:mb-3">
                             <h5 className="flex flex-wrap max-w-[400px] text-start text-sm sm:text-lg lg:text-2xl text-white font-semibold">{title}</h5>
-                            {/*<FaArrowRightLong className="hidden lg:flex items-center text-white text-2xl"  /> */}
                         </div>
                         <p className="hidden text-[10px] lg:pt-20 sm:flex md:text-sm lg:text-lg md:text-start text-gray-400">{description}</p>
                         <div className="flex flex-wrap mt-4">
@@ -50,7 +49,64 @@ const ProjectCard = ({ id, imgUrl, title, description, tag, link }: ProjectCardP
                         </div>
                     </div>
                 </div>
-            </GlowingCard>
+            </GlowingCard> */}
+
+            {/*New project card stuff*/}
+
+
+            <div className="flex flex-col py-8  justify-center mb-20 rounded-xl hover:bg-green-400/20">
+
+                <div className="grid grid-cols-12 relative">
+                    <div className="hidden md:flex col-span-2 mx-8 text-gray-400 font-semibold">
+                        <p>{date}</p>
+                    </div>
+
+                    <div className="col-span-10">
+
+                        {/* A container that spans 10 columns that contianes another containber with 2 columns, one for the image and another for the data */}
+                        <div className="grid grid-cols-2">
+
+                            {/* Image */}
+                            <div className="hidden md:block col-span-1 mr-8">
+                                <div className="w-full h-full rounded-2xl" style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}> </div>
+                            </div>
+
+
+
+
+                            {/* Data */}
+                            <div className="flex-col col-span-3 md:col-span-3 lg:col-span-1 mr-8">
+                               
+                                    <h5 className="flex mb-0.1 flex-wrap max-w-[400px] text-start text-md sm:text-lg lg:text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 to-emerald-700">{title}</h5>
+                                    <h5 className="flex mb-2 flex-wrap max-w-[400px] text-start text-sm sm:text-lg lg:text-xl text-gray-200 font-semibold">{subtitle}</h5>
+
+                                    {/*<FaArrowRightLong className="hidden lg:flex items-center text-white text-2xl"  /> */}
+                        
+                                <p className="text-gray-400 text-justify">{description}</p>
+                                <div className="flex flex-wrap mt-2">
+                                    {tag.map((tag) => (
+                                        <div key={tag} className="flex bg-transparent border-[1px] border-emerald-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mt-3">{tag}</div>
+                                    ))}
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
         </div>
     )
 }
